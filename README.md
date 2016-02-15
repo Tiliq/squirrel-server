647bb1a12411f3672768e3d3bfbdb814afe69697
Kevin Martin <kevin@neurs.com>
2016-02-14 19:38:59 -0500

    Rewrite squirrel-server completely, add more routes

--- README.md
+++ README.md
@@ -1,4 +1,274 @@
 # squirrel-server
-An update server for Squirrel (Squirrel.Mac / Squirrel.Windows) write in Node.js
+An update server for Squirrel (Squirrel.Mac / Squirrel.Windows) write in Node.js, specifically for Electron.
 
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Electron Auto-Updater
+To connect your Electron application to `squirrel-server`, simple set your feed url path to `/update/:platform/:arch/:currentVersion`.
+The same feed url will work for both Windows and Mac. At the moment, Linux is not supported in Electron.
+
+### Example
+```js
+const { autoUpdater }	= require('electron');
+const { version }		= require('./package.json');
+
+const feedUrl = `http://example.com/update/${process.platform}/${process.arch}/${version}`;
+
+autoUpdater.setFeedURL(feedUrl);
+```
+
+## Basic Setup
+When you set up your `squirrel-server`, the only required Environment Variable is `GITHUB_REPO`. All others are optional, but certain features may not work unless they are enabled.
+
+```bash
+# `Username/Repository`
+GITHUB_REPO="Tiliq/squirrel-server"
+```
+
+## Private GitHub Repositories
+[Create a GitHub Token](https://github.com/settings/tokens/new) for your account with the `repo` scope selected and export an Environment Variable called `GITHUB_TOKEN` with the value.
+
+```bash
+GITHUB_TOKEN="Sup3rS3cr3tT0k3n"
+```
+
+## GitHub Release Webhook
+Create a webhook on your repository that links to the `/webhook` path of your server. Every time you publish a new release, GitHub will ping the URL, causing the server to refresh it's data.
+
+### Webhook Timeout
+You may optionally specify how many seconds to wait after a ping before the server fetches data using `WEBHOOK_FETCH_TIMEOUT`. This is not recommended, as it's best to upload all artifacts before publishing a release.
+
+```bash
+# Wait 5 Seconds after publishing a new release
+WEBHOOK_FETCH_TIMEOUT=5
+
+# Fetch new information immediately
+WEBHOOK_FETCH_TIMEOUT=0
+```
+
+## Auto-Updater
+
+### Auto-Updater for Mac
+###### Endpoint URL: `/update/darwin/x64/:currentVersion`
+Requires `DARWIN_ZIP_FILE` Environment Variable.
+
+```bash
+DARWIN_ZIP_FILE="App-darwin-x64.zip"
+```
+
+If an update is available (the latest published release version is greater than `:currentVersion`), then this endpoint will return an object with information on the release. Otherwise, an HTTP status code `204` (No Content) will be returned.
+
+Example Response:
+```json
+{
+	"url": "http://example.com/download/v1.1.0/App-darwin-x64.zip",
+	"link": "https://github.com/SquirrelServer/example/releases/tag/v1.1.0",
+	"name": "Example App",
+	"notes": "Release Notes",
+	"pub_date": "2016-03-25T13:50:42Z"
+}
+```
+
+### Auto-Updater for Windows
+###### Endpoint URL: `/update/win32/:arch/:currentVersion`
+###### Sub-Endpoint URL: `/update/win32/:arch/:currentVersion/RELEASES`
+###### Sub-Endpoint URL: `/update/win32/:arch/:currentVersion/*-delta.nupkg`
+###### Sub-Endpoint URL: `/update/win32/:arch/:currentVersion/*-full.nupkg`
+Requires `WIN32_RELEASES_FILE` and `WIN32_NUPKG_FILE` Environment Variables.
+
+```bash
+# `{{arch}}` and `{{filename}}` will automatically get
+# replaced based on the value in the Sub-Endpoint URL.
+WIN32_RELEASES_FILE="{{arch}}-RELEASES"
+WIN32_NUPKG_FILE="{{arch}}-{{filename}}"
+```
+
+If the **Endpoint URL** is accessed directly, a 404 will occur.
+
+Electron's auto-updater (Squirrel.Windows) gets the **Endpoint URL** and appends `/RELEASES` to it which contains a list of NuPKG files. If an update is not available, all Sub-Endpoint URLs will return an HTTP status code `204` (No Content).
+
+## Download files directly
+Apart from this being an auto-update server, you may also request specific files for all of your platforms, architectures, and versions.
+
+```bash
+wget http://example.com/download/latest/App-linux-x64.zip
+```
+
+### Direct Download Endpoints
+
+###### URL: `/download/latest/:platform/:arch`
+Requires `LINUX_DIRECT_FILE`, `DARWIN_DIRECT_FILE`, and/or `WIN32_DIRECT_FILE` in order to correctly retrieve file.
+
+Redirects to `/download/:version/:platform/:arch`, specifying `:version` using the latest published tag.
+
+```bash
+wget http://example.com/download/latest/darwin/x64
+```
+
+###### URL: `/download/:version/:platform/:arch`
+Requires `LINUX_DIRECT_FILE`, `DARWIN_DIRECT_FILE`, and/or `WIN32_DIRECT_FILE` in order to correctly retrieve file.
+
+```bash
+# `{{arch}}` and `{{version}}` will automatically
+# get replaced based on the value in the Endpoint URL.
+LINUX_DIRECT_FILE="App-linux-{{arch}}-{{version}}.zip"
+DARWIN_DIRECT_FILE="App-darwin-x64.dmg"
+WIN32_DIRECT_FILE="{{arch}}-AppSetup.exe"
+```
+
+Redirects to `/download/:version/:filename`, specifying `:filename` using the environment variables to specify the filename.
+
+```bash
+wget http://example.com/download/v1.0.0/darwin/x64
+```
+
+###### URL: `/download/latest/:filename`
+Redirects to `/download/:version/:filename`, specifying `:version` using the latest published tag.
+
+```bash
+wget http://example.com/download/latest/AppSetup.exe
+```
+
+###### URL: `/download/:version/:filename`
+Downloads the specified filename and the specified version (tag name).
+
+```bash
+wget http://example.com/download/v1.0.0/AppSetup.exe
+```
+
+## API Endpoints
+
+###### URL: `/api/resolve`
+*Optional Query Parameters:*
+- `platform` - `darwin`, `win32`, `linux`
+- `arch` - `ia32`, `x64`
+
+Attempts to resolve the platform, architecture, and download url best suited for the current user based on the `User-Agent`.
+Platform (`platform`) and/or architecture (`arch`) may be passed through instead of attempting to "guess".
+
+Example Responses:
+```bash
+// wget http://example.com/api/resolve
+{
+	"platform": "darwin",
+	"arch": "x64",
+	"url": "http://example.com/download/latest/darwin/x64"
+}
+
+// wget http://example.com/api/resolve?platform=win32
+{
+	"platform": "win32",
+	"arch": "x64",
+	"url": "http://example.com/download/latest/win32/x64"
+}
+
+// wget http://example.com/api/resolve?platform=linux&arch=ia32
+{
+	"platform": "linux",
+	"arch": "ia32",
+	"url": "http://example.com/download/latest/linux/ia32"
+}
+```
+
+###### URL: `/api/versions`
+Returns a JSON array of release tag names
+
+Example Response:
+```bash
+{
+	"versions": ["v1.0.0", "v1.1.0", "v1.1.1"]
+}
+```
+
+###### URL: `/api/version/latest`
+Redirects to `/api/version/:version`, specifying `:version` using the latest published tag.
+
+###### URL: `/api/version/:version`
+Returns a JSON object with information on the current release and asset information for the release.
+
+Example Response:
+```bash
+{
+	"version": "v1.0.0",
+	"link": "http://github.com/Username/Repo/release/v1.0.0",
+	"name": "First Stable Release",
+	"notes": "Wooo!",
+	"pub_date": "2016-03-25T13:50:42Z",
+	"files": [
+		{
+			"name": "AppSetup.zip",
+			"url": "http://example.com/download/v1.0.0/AppSetup.zip",
+			"label": "AppSetup.zip",
+			"content_type": "application/zip",
+			"size": 28566258,
+			"download_count": 4
+		},
+		{
+			"name": "App-darwin.zip",
+			"url": "http://example.com/download/v1.0.0/App-darwin.zip",
+			"label": "App-darwin.zip",
+			"content_type": "application/zip",
+			"size": 39523293,
+			"download_count": 22
+		}
+	]
+}
+```
+
+## Environment Variable Replacement Tags
+The Environment Variables pertaining to the **Auto-Updater** and **Direct Download** endpoints are capable of mustache-style replacement tags.
+
+|  Environment Variable |                                 Description                                 |  Replacement Tags  |                               Used By                               |
+|:---------------------:|:---------------------------------------------------------------------------:|:------------------:|:-------------------------------------------------------------------:|
+|  `LINUX_DIRECT_FILE`  |         The filename for Linux file across releases (usually a Zip)         |  `arch`, `version` | `/download/latest/linux/:arch`<br/>`/download/:version/linux/:arch` |
+|  `DARWIN_DIRECT_FILE` |      The filename for Mac file across releases (usually a DMG or Zip)       |      `version`     |  `/download/latest/darwin/x64`<br/>`/download/:version/darwin/x64`  |
+|   `DARWIN_ZIP_FILE`   |               The filename for Mac Zip files across releases                |                    |                 `/update/darwin/x64/:currentVersion`                |
+| `WIN32_RELEASES_FILE` |         The filename for the Windows RELEASES file across releases          |       `arch`       |            `/update/win32/:arch/:currentVersion/RELEASES`           |
+|   `WIN32_NUPKG_FILE`  |            The filename for Windows NuPKG files across releases             | `arch`, `filename` |           `/update/win32/:arch/:currentVersion/:filename`           |
+|  `WIN32_DIRECT_FILE`  | The filename for Windows file across releases (usually an EXE, MSI, or Zip) |  `arch`, `version` | `/download/latest/win32/:arch`<br/>`/download/:version/win32/:arch` |
+
+## FAQ
+
+### What if I two or more possible files for the same platform and architecture?
+###### Example 1: `Setup.exe` and `Setup.msi`
+###### Example 2: `app-linux.deb` and `app-linux.rpm` and `app-linux.zip`
+Set your environment variable to include a custom `{{ext}}` replacement tag, then when you request the file using the appropriate `/download/:version/:platform/:arch` endpoint, specify `ext` as a query parameter.
+
+```bash
+LINUX_DIRECT_FILE="app-linux.{{ext}}" WIN32_DIRECT_FILE="Setup.{{ext}}" npm start
+```
+
+```bash
+wget http://example.com/download/latest/linux/x64?ext=rpm
+wget http://example.com/download/v1.0.0/win32/ia32?ext=msi
+```
+
+### How can I add two RELEASES files, one for `x64` and one for `ia32`?
+Set your `WIN32_RELEASES_FILE` environment variable to include the `{{arch}}` replacement tag and rename your RELEASES files accordingly.
+
+```bash
+# Have x64-RELEASES and ia32-RELEASES uploaded
+WIN32_RELEASES_FILE="{{arch}}-RELEASES" npm start
+```
+
+```bash
+wget http://example.com/update/win32/x64/v1.0.0/RELEASES
+wget http://example.com/update/win32/ia32/v1.0.0/RELEASES
+```
+
+### The filename includes the version number which is standard. How can I configure the server to find it properly?
+Set your Environment Variable to include the `{{version}}` replacement tag.
+
+```bash
+LINUX_DIRECT_FILE="app-{{version}}-linux-{{arch}}.zip"
+```
+
+```bash
+wget http://example.com/download/latest/linux/ia32
+```
+
+### How should I tag my releases?
+Valid [SemVer](http://semver.org/) version is required to properly match and compare versions.
+
+### How much disk space or memory should I establish for the machine?
+The smallest amount should be fine in most cases. `squirrel-server` does not download files, stream files, or store files. This saves the machine from having to worry about disk space and memory. `squirrel-server` works by finding where GitHub stores your file (on S3) and merely redirects all downloads to that location, using one-time use tokens if necessary (if using a private repo).
--- index.js
+++ index.js
@@ -7,29 +7,30 @@ const semver	= require('semver');
 const https		= require('https');
 const url		= require('url');
 const version	= require('./package.json').version;
-const userAgent = `Squirrel-Server (${version})`;
-const platforms	= ['darwin', 'win32'];
+const userAgent = `Squirrel-Server (v${version})`;
 
-const archSynonyms = {
-	x86: 'ia32',
-	x86_64: 'x64',
-	amd64: 'x64',
-	ia32: 'x86',
-	x64: 'amd64'
+const regexps = {
+	x64: /(?:(amd|x(?:(?:86|64)[_-])?|wow|win)64)[;\)]/,
+	ia32: /ia32(?=;)|(?:i[346]|x)86[;\)]/,
+	win32: /windows/,
+	darwin: /macintosh|mac\sos\sx/,
+	linux: /linux|debian|ubuntu|fedora|centos|redhat/
 };
 
-let latest, auth;
+let releases, auth;
 
 if (process.env.GITHUB_TOKEN) {
 	auth = process.env.GITHUB_TOKEN + ':';
 }
 
-function getLatestRelease() {
+function getAllReleases(page, tmp) {
 	return new Promise((resolve, reject) => {
+		page = page || 1;
+
 		const options = {
+			auth,
 			hostname: 'api.github.com',
-			path: `/repos/${process.env.GITHUB_REPO}/releases/latest`,
-			auth: auth,
+			path: `/repos/${process.env.GITHUB_REPO}/releases?page=${page}`,
 			headers: {
 				'User-Agent': userAgent
 			}
@@ -40,122 +41,318 @@ function getLatestRelease() {
 
 			res.setEncoding('utf8');
 			res.on('data', str => data += str);
-			res.on('end', () => resolve(JSON.parse(data)));
+			res.on('end', () => resolve({
+				link: res.headers.link,
+				data: JSON.parse(data).filter(r => !r.draft && !r.prerelease)
+			}));
 		});
 
 		req.on('error', e => reject(e));
 		req.end();
-	}).then(data => latest = data);
+	}).then(data => {
+		tmp = tmp || [];
+		tmp.push(...data.data);
+
+		if (/<([^>]+)>;\s*?rel="next"/.test(data.link)) {
+			return getAllReleases(++page, tmp);
+		} else {
+			releases = tmp;
+		}
+	});
 }
 
-function getFilename(query, value) {
-	let file = process.env.DARWIN_FILE;
-
-	for (let key in query) {
-		file = file.replace(`{{${key}}}`, value(query[key]));
+function processFilename(filename, vars) {
+	for (let [key, value] of vars) {
+		filename = filename.replace(`{{${key}}}`, value);
 	}
 
-	return file;
+	return filename;
 }
 
-function getAsset(filename) {
-	let i = 0,
-		l = latest.assets.length;
+function getReleaseVersionFromAsset(filename) {
+	for (let i = 0, l = releases.length; i < l; i++) {
+		const assets = releases[i].assets;
 
-	for (; i < l; i++) {
-		if (latest.assets[i].name === filename) {
-			return url.parse(latest.assets[i].url);
+		for (let j = 0, m = assets.length; j < m; j++) {
+			if (assets[j].name === filename) {
+				return releases[i].tag_name;
+			}
 		}
 	}
 }
 
-function getAssetByArch(filename, arch) {
-	if (!arch) {
-		return getAsset(filename);
+function getAssetFromReleaseVersion(version, filename) {
+	for (let i = 0, l = releases.length; i < l; i++) {
+		if (semver.neq(releases[i].tag_name, version)) {
+			continue;
+		}
+
+		const assets = releases[i].assets;
+
+		for (let j = 0, m = assets.length; j < m; j++) {
+			if (assets[j].name === filename) {
+				return url.parse(assets[j].url);
+			}
+		}
 	}
-
-	let asset = getAsset(`${arch}-${filename}`);
-
-	if (!asset) {
-		arch	= archSynonyms[arch];
-		asset	= getAsset(`${arch}-${filename}`);
-	}
-
-	return asset || getAsset(filename);
 }
 
 function getAssetUrl(asset) {
 	return new Promise((resolve, reject) => {
-		const options = {
+		const req = https.request({
+			auth,
 			method: 'HEAD',
 			hostname: asset.hostname,
 			path: asset.path,
-			auth: auth,
 			headers: {
 				Accept: 'application/octet-stream',
 				'User-Agent': userAgent
 			}
-		};
-
-		const req = https.request(options, res => resolve(res.headers.location));
+		}, res => resolve(res.headers.location));
 
 		req.on('error', e => reject(e));
 		req.end();
 	});
 }
 
-// Darwin releases
-router.get('/latest', function *getLatestRelease() {
-	if (!latest || !this.query.version) {
-		// wait for next request
-		this.status = 204;
-		return;
-	} else if (semver.lte(latest.tag_name, this.query.version)) {
-		this.status = 204;
-		return;
-	} else if (!~platforms.indexOf(this.query.platform)) {
-		this.status = 204;
-		return;
+function getFileFromPlatform(platform) {
+	if (platform === 'darwin') {
+		return process.env.DARWIN_DIRECT_FILE || process.env.DARWIN_ZIP_FILE;
+	} else if (platform === 'win32') {
+		return process.env.WIN32_DIRECT_FILE;
+	} else if (platform === 'linux') {
+		return process.env.LINUX_DIRECT_FILE;
 	}
+}
 
-	const filename	= getFilename(this.query);
-	const asset		= getAsset(filename);
-
-	if (!asset) {
-		this.status = 204;
-		return;
-	}
-
-	try {
-		this.body = {
-			url: yield getAssetUrl(asset),
-			link: latest.html_url,
-			name: latest.name,
-			notes: latest.body,
-			pub_date: latest.published_at
-		};
-	} catch (e) {
-		this.status = 204;
-		console.log(e);
+router.param('platform', function *handlePlatform(platform, next) {
+	if (~['darwin', 'win32', 'linux'].indexOf(platform)) {
+		this.platform = platform;
+		yield next;
+	} else {
+		this.status = 400;
 	}
 });
 
-// Win32 releases
-router.get('/latest/:filename', function *getReleasesFile() {
-	let asset = getAssetByArch(this.params.filename, this.query.arch);
+router.param('arch', function *handleArch(arch, next) {
+	if (~['ia32', 'x64'].indexOf(arch)) {
+		this.arch = arch;
+		yield next;
+	} else {
+		this.status = 400;
+	}
+});
 
-	if (!asset) {
+router.param('version', function *handleCurrentVersion(version, next) {
+	// Must be a valid semver version
+	if (semver.valid(version)) {
+		this.version = version;
+		yield next;
+	} else {
+		this.status = 400;
+	}
+});
+
+router.param('currentVersion', function *handleCurrentVersion(version, next) {
+	// Must be a valid semver version
+	if (!semver.valid(version)) {
+		this.status = 400;
+	} else if (semver.lte(releases[0].tag_name, version)) {
+		this.status = 204;
+	} else {
+		this.version = version;
+		yield next;
+	}
+});
+
+router.param('filename', function *handleFilename(filename, next) {
+	if (filename === 'RELEASES') {
+		yield next;
+	} else if (/-(delta|full)\.nupkg$/.test(filename)) {
+		yield next;
+	} else {
+		this.status = 400;
+	}
+});
+
+// Handle Darwin Auto-Updater
+router.get('/update/darwin/x64/:currentVersion', function *handleDarwin() {
+	const filename = process.env.DARWIN_ZIP_FILE;
+
+	this.body = {
+		url: `${this.origin}/download/${releases[0].tag_name}/${filename}`,
+		link: auth ? undefined : releases[0].html_url,
+		name: releases[0].name,
+		notes: releases[0].body,
+		pub_date: releases[0].published_at
+	};
+});
+
+// Handle Windows Auto-Updater
+// :filename will usually be RELEASES, *-delta.nupkg, or *-full.nupkg
+router.get('/update/win32/:arch/:currentVersion/:filename', function *handleWindows() {
+	let filename = this.params.filename;
+	let release;
+
+	if (this.params.filename === 'RELEASES') {
+		// RELEASES should only be fetched from the latest version
+		release = releases[0].tag_name;
+
+		if (process.env.WIN32_RELEASES_FILE) {
+			filename = processFilename(process.env.WIN32_RELEASES_FILE, this.params);
+		}
+	} else {
+		if (process.env.WIN32_NUPKG_FILE) {
+			filename = processFilename(process.env.WIN32_NUPKG_FILE, {
+				filename,
+				arch: this.arch,
+				platform: this.platform
+			});
+		}
+
+		// *-delta.nupkg and *-full.nupkg can come from previous versions
+		release = getReleaseVersionFromAsset(filename);
+
+		if (!release) {
+			this.status = 204;
+
+			return;
+		}
+	}
+
+	this.redirect(router.url('download', {
+		version: release,
+		file: filename
+	}));
+});
+
+router.get('/download/latest/:file', function *downloadLatestFile() {
+	this.redirect(router.url('download', {
+		version: releases[0].tag_name,
+		file: this.params.filename
+	}));
+});
+
+router.get('/download/latest/:platform/:arch', function *downloadLatestFile() {
+	this.redirect(router.url('download-file', {
+		version: releases[0].tag_name,
+		platform: this.platform,
+		arch: this.arch
+	}));
+});
+
+router.get('download-file', '/download/:version/:platform/:arch', function *downloadVersionedFile() {
+	let file = getFileFromPlatform(this.platform);
+
+	if (!file) {
 		this.status = 404;
+
 		return;
 	}
 
-	const assetUrl = yield getAssetUrl(asset);
+	file = processFilename(file, this.params);
 
-	this.redirect(assetUrl);
+	this.redirect(router.url('download', {
+		file,
+		version: this.version
+	}));
+});
+
+router.get('download', '/download/:version/:file', function *downloadVersionedFile() {
+	const asset = getAssetFromReleaseVersion(this.version, this.params.file);
+
+	if (asset) {
+		const assetUrl = yield getAssetUrl(asset);
+		this.redirect(assetUrl);
+	} else {
+		this.status = 404;
+	}
+});
+
+router.get('/api/versions', function *listVersions() {
+	const versions = [];
+	releases.forEach(r => versions.push(r.tag_name));
+	this.body = { versions };
+});
+
+router.get('/api/version/latest', function *getLatestVersionInfo() {
+	this.redirect(router.url('version', releases[0].tag_name));
+});
+
+router.get('version', '/api/version/:version', function *getVersionInfo() {
+	let release;
+
+	for (let i = 0, l = releases.length; i < l; i++) {
+		if (semver.eq(releases[i].tag_name, this.version)) {
+			release = releases[i];
+			break;
+		}
+	}
+
+	if (!release) {
+		this.status = 404;
+
+		return;
+	}
+
+	this.body = {
+		version: this.version,
+		link: auth ? undefined : release.html_url,
+		name: release.name,
+		notes: release.body,
+		pub_date: release.published_at,
+		files: release.assets.map(asset => {
+			return {
+				name: asset.name,
+				url: `${this.origin}/download/${this.version}/${asset.name}`,
+				label: asset.label,
+				content_type: asset.content_type,
+				size: asset.size,
+				download_count: asset.download_count
+			};
+		})
+	};
+});
+
+router.get('/api/resolve', function *resolveDownloadInfo() {
+	const ua	= this.headers['user-agent'].toLowerCase();
+	const base	= `${this.origin}/download/latest`;
+	const info	= {
+		platform: this.query.platform,
+		arch: this.query.arch
+	};
+
+	if ((!info.platform || !info.arch) && regexps.darwin.test(ua)) {
+		info.platform	= 'darwin';
+		info.arch		= 'x64';
+	} else {
+		if (!info.platform && regexps.win32.test(ua)) {
+			info.platform = 'win32';
+		} else if (!info.platform && regexps.linux.test(ua)) {
+			info.platform = 'linux';
+		}
+
+		if (!info.arch && regexps.x64.test(ua)) {
+			info.arch = 'x64';
+		} else if (!info.arch && regexps.ia32.test(ua)) {
+			info.arch = 'ia32';
+		} else if (!info.arch) {
+			info.suggestedArch = 'ia32';
+		}
+	}
+
+	if (info.platform && info.arch) {
+		info.url = `${base}/${info.platform}/${info.arch}`;
+	} else if (info.platform && info.suggestedArch) {
+		info.suggestedUrl = `${base}/${info.platform}/${info.suggestedArch}`;
+	}
+
+	this.body = info;
 });
 
 // Update to the latest version
-router.post('/webhook', () => {
+let timeout;
+router.post('/webhook', function *handleWebhook() {
 	this.status = 200;
 
 	if (this.headers['x-github-event'] !== 'ReleaseEvent') {
@@ -163,11 +360,16 @@ router.post('/webhook', () => {
 	}
 
 	if (process.env.WEBHOOK_FETCH_TIMEOUT <= 0) {
-		latest = this.request.body.release;
+		yield getAllReleases();
 	} else {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+
 		// The release may not have assets if it's published before they are
-		// upload, so let's wait WEBHOOK_FETCH_TIMEOUT minutes.
-		setTimeout(getLatestRelease, process.env.WEBHOOK_FETCH_TIMEOUT * 1000);
+		// uploaded, so let's wait WEBHOOK_FETCH_TIMEOUT seconds.
+		const seconds	= process.env.WEBHOOK_FETCH_TIMEOUT * 1000;
+		timeout			= setTimeout(getAllReleases, seconds);
 	}
 });
 
@@ -176,4 +378,4 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(process.env.PORT || 8000);
-getLatestRelease();
+getAllReleases();
